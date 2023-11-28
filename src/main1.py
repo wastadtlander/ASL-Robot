65cefcaf763727055b17fa93215d6a20e68cd59b
mayanicolek <131313635+mayanicolek@users.noreply.github.com>
2023-11-28 16:06:18 -0500

    Add files via upload

---
+++ src/main1.py
@@ -0,0 +1,83 @@
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
+import argparse
+import cv2
+import mediapipe as mp
+from keras.models import load_model
+import numpy as np
+import pandas as pd
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-m', '--model', 
+                    type=str, 
+                    choices=['elu', 'exponential', 'relu', 'selu', 'sigmoid', 'softplus', 'softsign', 'tanh'], 
+                    default='elu')
+args = parser.parse_args()
+
+model = load_model("smnist.keras")
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.5)
+
+cap = cv2.VideoCapture(0)
+
+# label_mapping = {
+#     0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F',
+#     6: 'G', 7: 'H', 8: 'I', 9: 'K', 10: 'L', 11: 'M',
+#     12: 'N', 13: 'O', 14: 'P', 15: 'Q', 16: 'R', 17: 'S',
+#     18: 'T', 19: 'U', 20: 'V', 21: 'W', 22: 'X', 23: 'Y'
+# }
+
+label_mapping = {0: 'A', 1: 'B', 2: 'C'}
+
+padding = 50
+
+while True:
+    ret, frame = cap.read()
+    cv2.imshow('Sign Language Detection', frame)
+    if not ret:
+        break
+
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    results = hands.process(rgb_frame)
+
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            bounding_box = cv2.boundingRect(np.array([[(lm.x * frame.shape[1], lm.y * frame.shape[0]) for lm in hand_landmarks.landmark]]).astype(np.int32))
+            x, y, w, h = bounding_box
+            if x >= 0 and y >= 0:
+                padding_x = padding
+                padding_y = padding
+                
+                x = max(0, x - padding_x)
+                y = max(0, y - padding_y)
+                w += 2 * padding_x
+                h += 2 * padding_y
+                
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                model_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                model_frame = model_frame[y:y + h, x:x + w]
+                model_frame = cv2.resize(model_frame, (28, 28))
+                # cv2.imshow('Sign Language Detection', model_frame)
+                model_frame = pd.DataFrame(model_frame.flatten()).T
+                model_frame = (model_frame.values / 255.0).reshape(-1, 28, 28, 1)
+                predicted_sign = model.predict(model_frame)
+                predicted_label = np.argmax(predicted_sign)
+                top3_labels = np.argsort(predicted_sign, axis=1)[0][-3:][::-1]
+                top3_values = np.take(predicted_sign, top3_labels)
+                top3_predictions = [(label_mapping.get(label, "Unknown"), value) for label, value in zip(top3_labels, top3_values)]
+                # if top3_labels[0] == 2 and top3_values[0] >= .9:
+                #     break;
+                print("Top 3 Predictions:", top3_predictions)
+
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ src/model1.py
@@ -0,0 +1,117 @@
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPool2D, Flatten, Dropout, BatchNormalization
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import LabelBinarizer
+import pandas as pd
+import numpy as np
+
+''' Loads and preprocesses data. '''
+
+# All letters
+
+# train = pd.read_csv("./data/sign_mnist_train.csv")
+# test = pd.read_csv("./data/sign_mnist_test.csv")
+
+# labelBinarizer = LabelBinarizer()
+# trainLabels = labelBinarizer.fit_transform(train["label"])
+# testLabels = labelBinarizer.fit_transform(test["label"])
+
+# trainAttributes = train.drop(labels = ["label"], axis=1)
+# testAttributes = test.drop(labels = ["label"], axis=1)
+
+# Filtered set
+
+train = pd.read_csv("./data/sign_mnist_train.csv")
+test = pd.read_csv("./data/sign_mnist_test.csv")
+
+filtered_train = train[train['label'].isin([0, 1, 2])]
+filtered_test = test[test['label'].isin([0, 1, 2])]
+
+labelBinarizer = LabelBinarizer()
+trainLabels = labelBinarizer.fit_transform(filtered_train["label"])
+testLabels = labelBinarizer.fit_transform(filtered_test["label"])
+
+trainAttributes = filtered_train.drop(labels=["label"], axis=1)
+testAttributes = filtered_test.drop(labels=["label"], axis=1)
+
+trainAttributes /= 255
+testAttributes /= 255
+
+trainAttributes = trainAttributes.values.reshape(-1, 28, 28, 1)
+testAttributes = testAttributes.values.reshape(-1, 28, 28, 1)
+
+datagen = ImageDataGenerator(
+    rotation_range=10,
+    zoom_range = 0.1, 
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+)
+
+datagen.fit(trainAttributes)
+
+''' 
+    Building the CNN. 
+    Using relu as activation function for entire CNN except output. 
+    Using adam optimizer.
+    Using categorical crossentropy loss function.
+    Running for 20 epochs.
+'''
+
+model = Sequential()
+model.add(Conv2D(8, (5, 5), strides=1, padding="same", activation="relu", input_shape=(28, 28, 1)))
+model.add(BatchNormalization())
+model.add(MaxPool2D((3, 3), strides=3, padding="same"))
+model.add(Conv2D(16, (5, 5), strides=1, padding="same", activation="relu"))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+model.add(MaxPool2D((3, 3), strides=3, padding="same"))
+model.add(Conv2D(32, (5, 5), strides=1, padding="same", activation="relu"))
+model.add(BatchNormalization())
+model.add(MaxPool2D((3, 3), strides=3, padding="same"))
+model.add(Flatten())
+model.add(Dense(units=32, activation="relu"))
+# model.add(Dropout(0.3))
+model.add(Dense(units=3, activation="softmax"))
+model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+model.summary()
+
+history = model.fit(datagen.flow(trainAttributes, trainLabels, batch_size=128), epochs=25, validation_data=(testAttributes, testLabels))
+
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.title('Training and Validation Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+plt.savefig("models/relu/loss_plot.png")
+plt.show()
+
+plt.plot(history.history['accuracy'], label='Training Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.title('Training and Validation Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.savefig("models/relu/accuracy_plot.png")
+plt.show()
+
+predictions = model.predict(testAttributes)
+predicted_labels = labelBinarizer.inverse_transform(predictions)
+true_labels = labelBinarizer.inverse_transform(testLabels)
+cm = confusion_matrix(true_labels, predicted_labels)
+
+plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Blues)
+plt.title("Confusion Matrix")
+plt.colorbar()
+classes = [str(i) for i in range(3)]
+tick_marks = np.arange(len(classes))
+plt.xticks(tick_marks, classes, rotation=45)
+plt.yticks(tick_marks, classes)
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+plt.savefig("models/relu/confusion_matrix.png")
+plt.show()
+
+model.save("smnist.keras")
